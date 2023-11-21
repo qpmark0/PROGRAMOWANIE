8608f70c3e9ca2f33c2aa208966bf9561000e325
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-11-21 16:42:19 +0100

    wqd23fwdwsws2sw2

---
+++ programowanie/string_console_app/string_console_app.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+void task1()
+{
+    char signFromUser;
+    //signFromUser = 'a';
+    cout << "Podaj znak" << endl;
+    cin >> signFromUser;
+    cout << "POda³eœ znak " << signFromUser;
+    //if (signFromUser >= 97 && signFromUser <=122)
+    if (signFromUser >= 'a' && signFromUser <= 'z')
+        cout << "Znak jest ma³¹ liter¹ alfabetu" << endl;
+    else
+        cout << "Znak nie jest ma³¹ liter¹ alfabetu" << endl;
+
+
+}
+
+int main()
+{
+    task1();
+}
