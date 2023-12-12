b6f4d21a5cc02aac1eb38f0faa55d7369bdf26c6
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-12 16:45:21 +0100

    errghergeyr

---
+++ programowanie/array_console_application/array_console_application.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void task1()
+{
+    const int SIZE_OF_ARRAY = 5;
+    int numbers[SIZE_OF_ARRAY];
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+    {
+        cout << "Podaj liczbe" << endl;
+        cin >> numbers[i];
+    }
+    for ( i = SIZE_OF_ARRAY - 1; i >= 0; i--)
+    {
+        cout << "Podana liczba " << numbers[i] << endl;
+    }
+}
+int main()
+{
+    task1();
+}
+
+   
