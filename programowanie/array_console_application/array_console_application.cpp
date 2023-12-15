a3ad8c5dea7e8aa6942a840cbeb33c9a04349a43
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-15 13:17:12 +0100

    jhkfgytjfgjhfgh

--- programowanie/array_console_application/array_console_application.cpp
+++ programowanie/array_console_application/array_console_application.cpp
@@ -15,9 +15,25 @@ void task1()
         cout << "Podana liczba " << numbers[i] << endl;
     }
 }
+
+void task2()
+{
+    const int UPPER_RANGE = 10;
+    const int LOWER_RANGE = 5;
+    const int SIZE_OF_ARRAY = 5;
+    int numbers[SIZE_OF_ARRAY];
+    srand(time(NULL));
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+    {
+        numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+    }
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
+        cout << numbers[i] << "," << endl;
+        
+}
 int main()
 {
-    task1();
+    task2();
 }
 
    
