ed6cdab0d03ea4ececca7320c7db304a5673134e
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-05-20 11:24:11 +0200

    uigtyutygh

--- programowanie/zadania/zadania.cpp
+++ programowanie/zadania/zadania.cpp
@@ -1,128 +1,149 @@
 #include <iostream>
 using namespace std;
 
-    class Count {
-    private:
-        double number;
-    public:
+class Count {
+private:
+    double number;
+public:
+    void add(double addingNumber) {
+        number += addingNumber;
+    }
 
-        void add(double addingNumber)
-        {
-            number += number + addingNumber;
-        };
+    void substract(double substractNumber) {
+        number -= substractNumber;
+    }
 
-        void substract(double substractNumber)
-        {
-            number -= number - substractNumber;
+    Count() : number(0) {}
+
+    Count(double oneParameter) : number(oneParameter) {}
+
+    double getNumber() const {
+        return number;
+    }
+};
+
+class Sumator {
+private:
+    int numbers[10];
+public:
+    Sumator() {
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = 0;
         }
+    }
 
-       Count()
-        {
-            number = 0;
+    Sumator(int maxValue) {
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = rand() % (maxValue + 1);
         }
+    }
 
-        Count(double oneParameter) :number(oneParameter) {}
-
-        double getNumber() const
-        {
-            return number;
+    double Sum() const {
+        double sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += numbers[i];
         }
-    };
+        return sum;
+    }
 
-    class Sumator
-    {
-    private:
-        int numbers[10];
-    public:
-        Sumator()
-        {
-            for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
-            {
-                numbers[i] = 0.0;
-            }
-           
-        }
-
-        Sumator(int maxValue)
-        {
-            for (int i = 0; i < sizeof(numbers)/sizeof(numbers[0]); i++)
-            {
-                numbers[i] = (rand() % (maxValue + 1));
-            }
-        }
-
-        double Sum()
-        {
-            double Sum = 0;
-            for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++)
-            {
-                Sum = Sum + numbers[i];
-            }
-            return Sum;
-        }
-
-        double SumDivide3()
-        {
-            double sum = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                if (numbers[i] % 3 == 0)
-                {
-                    sum = sum + numbers[i];
-                }
-                return sum;
+    double SumDivide3() const {
+        double sum = 0;
+        for (int i = 0; i < 10; i++) {
+            if (numbers[i] % 3 == 0) {
+                sum += numbers[i];
             }
         }
+        return sum;
+    }
 
-        void showInfo()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                cout << i << "." << numbers[i] << endl;
-            }
+    void showInfo() const {
+        for (int i = 0; i < 10; i++) {
+            cout << i << ". " << numbers[i] << endl;
+        }
+    }
+
+    void showElementsInRange(int lowIndex, int highIndex) const {
+        if (lowIndex < 0) {
+            lowIndex = 0;
         }
 
-        void showElementsInRange(int lowIndex, int highIndex) 
-        {
-            if (lowIndex < 0) {
-                lowIndex = 0;
-            }
-
-            if (highIndex >= sizeof(numbers) / sizeof(numbers[0]))
-            {
-                highIndex = sizeof(numbers) / sizeof(numbers[0]) - 1;
-            }
-
-            for (int i = lowIndex; i <= highIndex; i++) 
-            {
-                cout << "numbers[" << i << "] = " << numbers[i] <<endl;
-            }
+        if (highIndex >= 10) {
+            highIndex = 9;
         }
-    };
 
-int main()
-{
-    //ex1
+        for (int i = lowIndex; i <= highIndex; i++) {
+            cout << "numbers[" << i << "] = " << numbers[i] << endl;
+        }
+    }
+};
+
+class Person {
+private:
+    string firstName;
+    string lastName;
+    int age;
+
+public:
+    Person(string firstName, string lastName, int age)
+        : firstName(firstName), lastName(lastName), age(age) {}
+
+    void Print() const {
+        cout << "First Name: " << firstName << ", Last Name: " << lastName << ", Age: " << age << endl;
+    }
+};
+
+class Book {
+private:
+    string title;
+    Person author;
+    int publicationYear;
+
+public:
+    Book(string title, Person author, int publicationYear)
+        : title(title), author(author), publicationYear(publicationYear) {}
+
+    void Print() const {
+        cout << "Title: " << title << ", Author: ";
+        author.Print();
+        cout << "Publication Year: " << publicationYear << endl;
+    }
+};
+
+int main() {
+    // ex1
     Count counter;
     counter.add(5.0);
 
-    cout << "current number:" << counter.getNumber() << "." << endl;
-    return 0;
-    //ex2
+    cout << "Current number: " << counter.getNumber() << "." << endl;
+
+    // ex2
     Sumator cat;
     Sumator dog(100);
 
     cout << "Elements of sumator without parameters: " << endl;
     cat.showInfo();
 
-    cout << "Elements of sumator with paramerters: " << endl;
-    dog.showInfo;
+    cout << "Elements of sumator with parameters: " << endl;
+    dog.showInfo();
 
-    cout << "Elements of sumator n range 2-8" << endl;
-    dog.showElementsInRange(2, 8);
+    cout << "Elements of sumator in range 6-9" << endl;
+    dog.showElementsInRange(6, 9);
 
-    cout << "sum of elements of second sumator(with parameters)" << dog.Sum() << endl;
-    cout << "sum of elements of second sumator (with parameters) that can devide by 3. ", dog.SumDivide3 << endl;
+    cout << "Sum of elements of second sumator (with parameters): " << dog.Sum() << endl;
+    cout << "Sum of elements of second sumator (with parameters) that can be divided by 3: " << dog.SumDivide3() << endl;
 
+    // ex3
+    Person author1("Sara", "Dor", 49);
+    Person author2("Jane", "Some", 26);
+
+    Book book1("C++ Programming", author1, 2016);
+    Book book2("Advanced C++", author2, 2008);
+
+    cout << "Book 1 Details:" << endl;
+    book1.Print();
+
+    cout << "Book 2 Details:" << endl;
+    book2.Print();
+
+    return 0;
 }
-
