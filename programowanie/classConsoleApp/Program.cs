4389faf252e3f996bb9e6d6ffc7572c12ba9de78
q <q>
2024-10-11 11:38:14 +0200

    q3r34t34t3

---
+++ programowanie/classConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿//ListCollectionConsoleApp
+
+//C++:
+//int intArray[10];
+
+//C#:
+//int[] intArray = new int[10];
+//intArray[0] = 9;
+
+
+using System.Reflection.Metadata.Ecma335;
+
+List<int> intsCollection = new List<int>();
+intsCollection.Add(15);
+intsCollection.Add(18);
+
+
+intsCollection.Add(5);
+
+Console.WriteLine(intsCollection[0]);
+Console.WriteLine(intsCollection[1]);
+Console.WriteLine(intsCollection[2]);
+
+intsCollection.Remove(1);
+
+Console.WriteLine(intsCollection[0]);
+Console.WriteLine(intsCollection[1]);
+
+intsCollection.Add(12);
+intsCollection.Add(19);
+intsCollection.Add(4);
+
+Console.WriteLine("Elementy na kolekcji:");
+for (int i = 0; i < intsCollection.Count; i++)
+{
+    Console.WriteLine(intsCollection[i]);
+}
+
+List<int> intsGreat10Collection=new List<int>();
+for (int i = 0;i < intsCollection.Count; i++)
+{
+    if (intsCollection[i] > 10)
+        intsGreat10Collection.Add(intsCollection[1]);
+}
+
+Console.WriteLine("Elementy na kolekcji wiekszych niz 10:");
+    for (int i = 0; i<= intsGreat10Collection.Count;i++)
+{
+    Console.WriteLine(intsGreat10Collection[i]);
+}
---
+++ programowanie/structconsoleapp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+struct Point
+{
+    public int x;
+    public int y;
+};
+
+
+Point firstPoint = new Point();
+firstPoint;
+secondPoint;
+firstPoint.x = 6;
+secondPoint.x = 6;
+Console.WriteLine(firstPoint.x);
+Console.WriteLine(secondPoint.x);
