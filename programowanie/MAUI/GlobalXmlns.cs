ea22ac7eb3f2729ea6c79e3d59a26d5139792cd7
Sala213 <Sala213@ezse.pl>
2025-09-08 15:01:02 +0200

    fnfgnj

---
+++ programowanie/MAUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUI.Pages")]
---
+++ programowanie/MAUI1/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUI1")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MAUI1.Pages")]
